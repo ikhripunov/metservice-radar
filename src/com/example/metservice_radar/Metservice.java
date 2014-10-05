9b87a74327926bc55110b0bba8d9ee774c8a292b
ikhripunov <ihripunov@gmail.com>
2014-10-05 16:40:21 +1300

    Added ability  to load image from URL

--- src/com/example/metservice_radar/Metservice.java
+++ src/com/example/metservice_radar/Metservice.java
@@ -1,7 +1,15 @@
 package com.example.metservice_radar;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.URL;
 
 public class Metservice extends Activity {
     /**
@@ -10,6 +18,40 @@ public class Metservice extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AsyncTask task = new DownloadImageTask().execute("http://www.metservice.com/IcePics/ob/249a-148dd775b60-148dd7a7f65.RadarAUCKLAND.jpeg");
         setContentView(R.layout.main);
+    }
+
+    protected Bitmap doInBackground(String... urls) {
+        String urldisplay = urls[0];
+        Bitmap mIcon11 = null;
+        try {
+            mIcon11 = BitmapFactory.decodeStream(new URL(urldisplay).openConnection().getInputStream());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return mIcon11;
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            ImageView img = (ImageView) findViewById(R.id.imageView1);
+            img.setImageBitmap(result);
+        }
     }
 }
