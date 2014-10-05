b9f9ad8317f3fdcb66f08ff9c5533a070e93da31
ikhripunov <ihripunov@gmail.com>
2014-10-05 18:29:46 +1300

    Demo
    On start loads JSON, then latest full-sized radar image

---
+++ src/com/example/metservice_radar/Constants.java
@@ -0,0 +1,9 @@
+package com.example.metservice_radar;
+
+/**
+ * Created by ihrip_000 on 5/10/2014.
+ */
+public class Constants {
+    public static final String PUBLIC_DATA_RADAR = "/publicData/rainRadarAuckland_2h_7min_300K";
+    public static final String URL = "http://www.metservice.com";
+}
--- src/com/example/metservice_radar/Metservice.java
+++ src/com/example/metservice_radar/Metservice.java
@@ -7,9 +7,11 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
-
-import java.io.InputStream;
-import java.net.URL;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
 
 public class Metservice extends Activity {
     /**
@@ -18,40 +20,58 @@ public class Metservice extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AsyncTask task = new DownloadImageTask().execute("http://www.metservice.com/IcePics/ob/249a-148dd775b60-148dd7a7f65.RadarAUCKLAND.jpeg");
         setContentView(R.layout.main);
+        new GetJsonDataTask().execute();
     }
 
-    protected Bitmap doInBackground(String... urls) {
-        String urldisplay = urls[0];
-        Bitmap mIcon11 = null;
-        try {
-            mIcon11 = BitmapFactory.decodeStream(new URL(urldisplay).openConnection().getInputStream());
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        return mIcon11;
-    }
-
-    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-
+    public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
         protected Bitmap doInBackground(String... urls) {
-            String urldisplay = urls[0];
-            Bitmap mIcon11 = null;
+            Bitmap image = null;
             try {
-                InputStream in = new java.net.URL(urldisplay).openStream();
-                mIcon11 = BitmapFactory.decodeStream(in);
+                image = BitmapFactory.decodeStream(new java.net.URL(urls[0]).openStream());
             } catch (Exception e) {
                 Log.e("Error", e.getMessage());
                 e.printStackTrace();
             }
-            return mIcon11;
+            return image;
         }
 
         protected void onPostExecute(Bitmap result) {
             ImageView img = (ImageView) findViewById(R.id.imageView1);
             img.setImageBitmap(result);
+            img.setAdjustViewBounds(true);
+            img.setMinimumHeight(600);
+            img.setMinimumWidth(600);
+        }
+    }
+
+    public class GetJsonDataTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                String response = makeGetRequest(Constants.URL + Constants.PUBLIC_DATA_RADAR);
+                JSONArray array = new JSONArray(response);
+                new DownloadImageTask().execute(Constants.URL + array.getJSONObject(0).get("url"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        public String makeGetRequest(String path) throws Exception {
+            //instantiates httpclient to make request
+            DefaultHttpClient httpclient = new DefaultHttpClient();
+
+            //url with the post data
+            HttpGet httpGet = new HttpGet(path);
+
+            //sets a request header so the page receving the request
+            //will know what to do with it
+            httpGet.setHeader("Accept", "application/json, text/javascript, */*; q=0.01");
+
+            //Handles what is returned from the page
+            ResponseHandler responseHandler = new BasicResponseHandler();
+            return (String) httpclient.execute(httpGet, responseHandler);
         }
     }
 }
